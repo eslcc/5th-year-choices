ef7484249e9578003c2ecc5c83cde51bf73abd33
Marks Polakovs <marks@markspolakovs.me>
2017-04-13 17:59:35 +0200

    Use classNames helper for all the things

--- src/ErrorBar.tsx
+++ src/ErrorBar.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {ChoiceValues, checkValidity} from './constants/choices';
 import {I18nField} from './constants/i18n';
 
+const classNames = require('classnames');
+
 function isError(err: I18nField | number): err is I18nField {
     return err.hasOwnProperty('en');
 }
@@ -11,7 +13,7 @@ export default ({ values }: { values: ChoiceValues }) => {
     const valid = typeof errorOrTotal === 'number';
 
     return (
-        <div className={`error-bar ${valid ? 'valid' : 'invalid'}`}>
+        <div className={classNames('error-bar', valid ? 'valid' : 'invalid')}>
             <span className="label">{valid ? `Valid. Total periods: ${errorOrTotal}` : 'Invalid'}. </span>
             {isError(errorOrTotal) && errorOrTotal.en}
         </div>
